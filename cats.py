6a7d476837c610a5ccbe27524832dd6c6422d953
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2022-02-04 22:13:52 -0500

    Add memoize

--- cats.py
+++ cats.py
@@ -1,3 +1,4 @@
+# Class to create infix operators
 class Infix:
     def __init__(self, function):
         self.function = function
@@ -12,24 +13,31 @@ class Infix:
     def __call__(self, value1, value2):
         return self.function(value1, value2)
 
+#####################
+# === Chapter 1 === #
+#####################
+
+# Identify function
 def id(f):
     return f
 
+# Function composition operator
 def _after(girl,friend):
     return lambda *x : girl(friend(*x))
 
 after = Infix(_after)
 
-def f(x,y):
-    return x + y
+#####################
+# === Chapter 2 === #
+#####################
 
-def g(x):
-    return x * 2
-
-
-h = g |after| f
-
-print(h(1,1))
-
-## Output:
-## 4
+# Memoize any function f. 
+# Requires that f's arguments can be cast into a string. 
+def memoize(f):
+    cache = {}
+    def memoizedF(*x):
+        argkey = str(*x)
+        if argkey not in cache:
+            cache[argkey] = f(*x)
+        return cache[argkey]
+    return memoizedF
