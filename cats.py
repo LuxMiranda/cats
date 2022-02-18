0922d3d0b11cebaa5c0f99ef41b668ab5f6f9e15
Lux Miranda <19409711+LuxMiranda@users.noreply.github.com>
2022-02-17 20:58:54 -0500

    Update

--- cats.py
+++ cats.py
@@ -1,3 +1,6 @@
+from __future__ import annotations
+from typing import Callable, Generic, TypeVar, Union
+
 # Class to create infix operators
 class Infix:
     def __init__(self, function):
@@ -13,26 +16,48 @@ class Infix:
     def __call__(self, value1, value2):
         return self.function(value1, value2)
 
-#####################
-# === Chapter 1 === #
-#####################
+ReturnType = TypeVar("ReturnType")
+Function = Callable[..., ReturnType]
+
+# Partial class for implementing partial function application
+# From: https://github.com/sagnibak/func_py/blob/master/curry.py
+class Partial(Generic[ReturnType]):
+    def __init__(
+            self, num_args: int, fn: Callable[..., ReturnType], *args, **kwargs
+    ) -> None:
+
+        self.num_args = num_args
+        self.fn       = fn
+        self.args     = args
+        self.kwargs   = kwargs
+
+    def __call__(self, *more_args, **more_kwargs) -> Union[Partial[ReturnType], ReturnType]:
+        all_args = self.args + more_args 
+        all_kwargs = dict(**self.kwargs, **more_kwargs)
+        num_args = len(all_args) + len(all_kwargs)
+        if num_args >= self.num_args:
+            return self.fn(*all_args, **all_kwargs)
+        else:
+            return Partial(self.num_args, self.fn, *all_args, **all_kwargs)
+
+    def __repr__(self):
+        return f"Partial({self.fn}, args={self.args}, kwargs={self.kwargs})"
+
+# Currying!
+def curry(num_args: int) -> Callable[[Callable[..., ReturnType]], Partial[ReturnType]]:
+    def decorator(fn: Callable[..., ReturnType]):
+        return Partial(num_args, fn)
+    return decorator
 
 # Identify function
 def id(f):
     return f
 
 # Function composition operator
-def _after(girl,friend):
-    return lambda *x : girl(friend(*x))
+@Infix
+def o(girl, friend):
+    return lambda *x : girl(friend(*x)) 
 
-after = Infix(_after)
-
-#####################
-# === Chapter 2 === #
-#####################
-
-# Memoize any function f. 
-# Requires that f's arguments can be cast into a string. 
 def memoize(f):
     cache = {}
     def memoizedF(*x):
